60b856ca91a04c6f5a6d56002fafba08eac49f82
nyg7 <yuricantox@gmail.com>
2026-04-28 14:55:43 -0300

    coments

--- main.py
+++ main.py
@@ -2,3 +2,23 @@ import os
 from tkinter.filedialog import askdirectory
 
 caminho = askdirectory(title="Selecione uma pasta")
+
+lista_arquivos = os.listdir(caminho)
+
+locais = {
+    "imagens": [".png", ".jpg"],
+    "planilhas": [".xlsx"],
+    "pdfs": ["pdf"],
+    "csv": [".csv"],
+}
+
+for arquivo in lista_arquivos:
+    # 01. Arquivo.pdf
+    nome, extensao = os.path.splittext(f"{caminho}/{arquivo}")
+    for pasta in locais:
+        if extensao in locais[pasta]:
+            if not os.path.exists(f"{caminho}/{pasta}"):
+                os.mkdir(f"{caminho}/{pasta}")
+            os.rename(f"{caminho}/{arquivo}", "{caminho}/{pasta}/{arquivo}")
+
+# add news dot
